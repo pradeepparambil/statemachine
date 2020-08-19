b5294268cef9ec0bddc81915872f4f16bc642377
pradeep.pp <pradeep.pp@gmail.com>
2020-08-19 14:21:02 -0400

    Initial Add.

---
+++ src/test/java/com/teksenz/statemachine/StatemachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teksenz.statemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatemachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
